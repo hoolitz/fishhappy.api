9816107035ccb0493093a3c9ee3b4ac8647c0965
alex yombo <lilkevinrobert@gmail.com>
2024-02-27 14:08:56 +0300

    Update the api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\CustomerController;
+use App\Http\Controllers\Api\CategoriesController;
+use App\Http\Controllers\Api\ProductsController;
+use App\Http\Controllers\Api\OrdersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,16 +14,25 @@ Route::group(['prefix' => 'cstmr'], function () {
     Route::post('/register', [RegisterController::class, 'register']);
     Route::post('/login', [LoginController::class, 'login']);
     Route::get('/',[CustomerController::class,'__invoke']);
+    Route::get("/categories", [CategoriesController::class, '__invoke']);
+    Route::get("/products", [ProductsController::class,"index"]);
+    Route::get("/products/{product}", [ProductsController::class,"show"]);
+    Route::get("/orders", [OrdersController::class,"index"]);
+    Route::post("/orders", [OrdersController::class,"store"]);
+    Route::get("/orders/{order}", [OrdersController::class,"show"]);
+
+
 
     Route::post("/change_password", "Api\ChangePasswordController");
     Route::post("/password/reset", "Api\ResetPasswordController");
     Route::post("/password/email", "Api\ForgotPasswordController");
-    Route::get("/categories", "Api\CategoriesController");
-    Route::get("/products", "Api\ProductsController@index");
-    Route::get("/products/{product}", "Api\ProductsController@show");
-    Route::get("/orders", "Api\OrdersController@index");
-    Route::get("/orders/{order}", "Api\OrdersController@show");
-    Route::post("/orders", "Api\OrdersController@store");
+
+
+
+//    Route::get("/products/{product}", "Api\ProductsController@show");
+//    Route::get("/orders", "Api\OrdersController@index");
+//    Route::get("/orders/{order}", "Api\OrdersController@show");
+//    Route::post("/orders", "Api\OrdersController@store");
     Route::post("/orders/{order}/cancel", "Api\OrderCancelController");
     Route::post("/orders/{order}/confirm", "Api\OrderConfirmController");
     Route::post("/products/{product}/favourites", "Api\ProductFavouriteController@store");
