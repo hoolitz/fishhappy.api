0400fce42d55e91b1a7ed534182a39b7c824c872
alex yombo <lilkevinrobert@gmail.com>
2024-03-26 13:39:06 +0300

    Update district api

--- app/District.php
+++ app/District.php
@@ -7,9 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class District extends Model
 {
     protected $fillable = [
-      'region_id',
-      'napa_district_id',
-      'name',
-      'postcode'
+        'napa_region_id',
+        'region_id',
+        'napa_district_id',
+        'name',
+        'postcode'
     ];
 }
--- database/seeds/DistrictSeeder.php
+++ database/seeds/DistrictSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\District;
 use App\Region;
 use Illuminate\Database\Seeder;
 
@@ -16,22 +17,23 @@ class DistrictSeeder extends Seeder
             $regionsId = Region::select(['id','napa_region_id'])->get();
             foreach ($regionsId as $key => $regionId) {
 
-                echo($regionsId);
+                //echo($regionsId);
+                $response = Http::withHeaders([
+                    'X-Napa-Api-Key' => env('NAPA_API_KEY', 'None'),
+                ])->get(env('NAPA_BASE_URL') .'frontend_api/api/pub/districts/'.$regionsId->napa_region_id);
 
-//                $response = Http::withHeaders([
-//                    'X-Napa-Api-Key' => env('NAPA_API_KEY', 'None'),
-//                ])->get(env('NAPA_BASE_URL') .'frontend_api/api/pub/districts/'.$regionsId);
-//
-//                if ($response->successful()) {
-//                    $regions = $response->json()['data'];
-//                    foreach ($regions as $region) {
-//                        Region::create([
-//                            'name' => $region['name'],
-//                            'napa_region_id' => $region['id'],
-//                            'postcode' => $region['postcode']
-//                        ]);
-//                    }
-//                }
+                if ($response->successful()) {
+                    $districts = $response->json()['data'];
+                    foreach ($districts as $district) {
+                        District::create([
+                            'region_id' => $regionsId->id,
+                            'name' => $district['name'],
+                            'napa_district_id' => $district['id'],
+                            'napa_region_id' => $regionsId->napa_region_id,
+                            'postcode' => $district['postcode']
+                        ]);
+                    }
+                }
             }
 
         } catch (Exception $e) {
